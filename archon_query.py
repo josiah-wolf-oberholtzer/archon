155bcb008da13e76590b02c53b95e451602d950e
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2022-08-01 09:42:20 -0400

    Reformat Python files

--- archon_query.py
+++ archon_query.py
@@ -1,34 +1,38 @@
-from multiprocessing.connection import Client
-import numpy as np
-import json as json
-import os, os.path
 import argparse
-import torch
-import pandas as pd
+import json as json
 import random
 
-from turtle import ycor
+import numpy as np
+import pandas as pd
+import torch
 from pythonosc import dispatcher, osc_server
 from pythonosc.udp_client import SimpleUDPClient
+
 
 # simple function to take json storage and convert to pandas df
 def dataframe_from_file(filename):
     with open(filename) as t:
         data = json.load(t)
         df = pd.DataFrame(data).T
-    database_as_df = dict(tuple((df.groupby('pitch'))))
+    database_as_df = dict(tuple((df.groupby("pitch"))))
     return database_as_df
+
 
 # simple function to convert pandas df into a tensor
 def tensor_dict_from_dataframe(database_as_df):
     tens_dict = {}
-    for k,v in database_as_df.items():
-        v_nopitch = v.drop(labels = 'pitch', axis = 1).drop(labels='rms', axis=1).drop(labels='flat', axis=1)
+    for k, v in database_as_df.items():
+        v_nopitch = (
+            v.drop(labels="pitch", axis=1)
+            .drop(labels="rms", axis=1)
+            .drop(labels="flat", axis=1)
+        )
         column_list = list(v_nopitch.columns)
         tens_dict[k] = torch.tensor(
-            v_nopitch.loc[:, column_list]
-            .to_numpy(dtype=np.float32))
+            v_nopitch.loc[:, column_list].to_numpy(dtype=np.float32)
+        )
     return tens_dict
+
 
 # processes information from supercollider into tensor
 def process_input(input_dict):
@@ -36,85 +40,116 @@ def process_input(input_dict):
     pitch = list(input_dict.items())[0][1].get("pitch")
     variance = list(input_dict.items())[0][1].get("variance")
     input_as_df = pd.DataFrame(input_dict).T
-    input_as_df = input_as_df.drop(labels='pitch', axis=1).drop(labels='rms', axis=1).drop(labels='flat', axis=1).drop(labels='variance', axis=1)
+    input_as_df = (
+        input_as_df.drop(labels="pitch", axis=1)
+        .drop(labels="rms", axis=1)
+        .drop(labels="flat", axis=1)
+        .drop(labels="variance", axis=1)
+    )
     column_list = list(input_as_df.columns)
-    in_t = torch.tensor(
-        input_as_df.loc[:, column_list]
-        .to_numpy(dtype=np.float32))
+    in_t = torch.tensor(input_as_df.loc[:, column_list].to_numpy(dtype=np.float32))
     return pitch, variance, in_t
 
+
 # use pytorch to find min. minkowski distance between input and db, return 25 closest matches
-# note cuda is not enabled here yet (this is being tested on a 2015 macbook pro with only about 100 ms latency)
+# note cuda is not enabled here yet
+# (this is being tested on a 2015 macbook pro with only about 100 ms latency)
 def closest_node(input_dataframe, database_dataframe, database_tensors, audiodir):
     pitch, variance, input_tensor = process_input(input_dataframe)
-    if pitch not in database_dataframe: pitch = "unpitched"
+    if pitch not in database_dataframe:
+        pitch = "unpitched"
     this_database_tensor = database_tensors.get(pitch)
     result = []
-    dist = torch.cdist(input_tensor, this_database_tensor, p=2).flatten() 
+    dist = torch.cdist(input_tensor, this_database_tensor, p=2).flatten()
     variety_ = random.randint(0, 10)
-    
+
     for i in range(25):
         if variety_ > float(variance):
             values, indices = torch.kthvalue(dist, i + 1)
-        else: values, indices = torch.kthvalue(dist, dist.size(dim=0) - (i + 1))
+        else:
+            values, indices = torch.kthvalue(dist, dist.size(dim=0) - (i + 1))
         result.append(
             format_result(
-                database_dataframe.get(pitch).iloc[indices.item()], 
-                pitch, 
-                audiodir))
-            
+                database_dataframe.get(pitch).iloc[indices.item()], pitch, audiodir
+            )
+        )
+
     return result
+
 
 # tidy up result so it matches google drive's horrible storage mandates
 def format_result(sample, pitch, target_audiodir):
     audiofile = sample.name
-    #Takeout-safe formatting only
-    if audiofile[:-6] != 'ms.wav': 
+    # Takeout-safe formatting only
+    if audiofile[:-6] != "ms.wav":
         audiofile = audiofile[:-4] + "ms.wav"
-    if (pitch != "unpitched"): 
-      oct = int(pitch[-1])
-      pitch = pitch.replace(str(oct), "")  
-      return_dir = target_audiodir + pitch + "/" + str(oct) + "/"   
-    else: 
-      return_dir = target_audiodir + pitch + "/"
-      cent = sample.get("cent")
-      flat = sample.get("flat")
-      rolloff = sample.get("rolloff")
-      if float(cent) > 4000.0: return_dir = return_dir + "high_cent/"
-      else: return_dir = return_dir + "low_cent/"
-      if float(flat) > 0.01: return_dir = return_dir + "high_flat/"
-      else: return_dir = return_dir + "low_flat/"
-      if float(rolloff) > 8000: return_dir = return_dir+ "high_rolloff/"
-      else: return_dir = return_dir + "low_rollof/"  
+    if pitch != "unpitched":
+        oct = int(pitch[-1])
+        pitch = pitch.replace(str(oct), "")
+        return_dir = target_audiodir + pitch + "/" + str(oct) + "/"
+    else:
+        return_dir = target_audiodir + pitch + "/"
+        cent = sample.get("cent")
+        flat = sample.get("flat")
+        rolloff = sample.get("rolloff")
+        if float(cent) > 4000.0:
+            return_dir = return_dir + "high_cent/"
+        else:
+            return_dir = return_dir + "low_cent/"
+        if float(flat) > 0.01:
+            return_dir = return_dir + "high_flat/"
+        else:
+            return_dir = return_dir + "low_flat/"
+        if float(rolloff) > 8000:
+            return_dir = return_dir + "high_rolloff/"
+        else:
+            return_dir = return_dir + "low_rollof/"
     return return_dir + audiofile
+
 
 # just to organize the osc server sequence - osc comes in from SC and the following sequence occurs
 def osc_handler(unused_addr, constants, args):
-    database_dataframe, database_tensors, audiodir, client = constants[0], constants[1], constants[2], constants[3]
+    database_dataframe, database_tensors, audiodir, client = (
+        constants[0],
+        constants[1],
+        constants[2],
+        constants[3],
+    )
     incoming = json.loads(args)
     node = closest_node(incoming, database_dataframe, database_tensors, audiodir)
     client.send_message("/node", node)
     print(node)
 
+
 if __name__ == "__main__":
 
     # IMPORTANT: please read for information around file locations as yours will differ
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip",
-        default="127.0.0.1", help="The ip to listen on")
-    parser.add_argument("--in_port",
-        type=int, default=5005, help="The port to listen on")
-    parser.add_argument("--out_port",
-        type=int, default=57120, help="The port to send to")      
-    parser.add_argument("--file",
-        default="/Users/marek_orpheus/Desktop/ARCHON_db/analysis_500ms.json", help="Location of analysis file (json)")
-    parser.add_argument("--audiodb",
-        default="/Users/marek_orpheus/Desktop/ARCHON_db/", help="Location of audio database")
+    parser.add_argument("--ip", default="127.0.0.1", help="The ip to listen on")
+    parser.add_argument(
+        "--in_port", type=int, default=5005, help="The port to listen on"
+    )
+    parser.add_argument(
+        "--out_port", type=int, default=57120, help="The port to send to"
+    )
+    parser.add_argument(
+        "--file",
+        default="/Users/marek_orpheus/Desktop/ARCHON_db/analysis_500ms.json",
+        help="Location of analysis file (json)",
+    )
+    parser.add_argument(
+        "--audiodb",
+        default="/Users/marek_orpheus/Desktop/ARCHON_db/",
+        help="Location of audio database",
+    )
     args = parser.parse_args()
 
-    # sets up Supercollider client and test message, if you don't see one in SC, start investigating
+    # sets up Supercollider client and test message.
+    # if you don't see one in SC, start investigating
     client = SimpleUDPClient(args.ip, args.out_port)
-    client.send_message("/superInterface", "ACK: Testing Connection w/ Python Server, Hold for OK")
+    client.send_message(
+        "/superInterface", "ACK: Testing Connection w/ Python Server, Hold for OK"
+    )
 
     # load up existing database and convert to tensors
     print("OK: loading database... this can take a minute if the database is huge!")
@@ -127,11 +162,16 @@ if __name__ == "__main__":
     # sets up Supercollider server and awaits OSC messages to /query
     dispatcher = dispatcher.Dispatcher()
 
-    dispatcher.map("/query", osc_handler, database_dataframe, database_tensors, args.audiodb, client)
+    dispatcher.map(
+        "/query",
+        osc_handler,
+        database_dataframe,
+        database_tensors,
+        args.audiodb,
+        client,
+    )
 
-
-    server = osc_server.ThreadingOSCUDPServer(
-        (args.ip, args.in_port), dispatcher)
+    server = osc_server.ThreadingOSCUDPServer((args.ip, args.in_port), dispatcher)
     print("Serving on {}".format(server.server_address))
     client.send_message("/superInterface", "OK: Python Server is Ready!")
     server.serve_forever()
